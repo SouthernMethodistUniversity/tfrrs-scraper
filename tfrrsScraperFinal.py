937d8da3410b82e56c69e081715b2c4a896fd4de
Mateo Langston Smith <mlangstonsmith@smu.edu>
2025-07-24 12:03:45 -0500

    Update tfrrsScraperFinal.py

--- tfrrsScraperFinal.py
+++ tfrrsScraperFinal.py
@@ -66,8 +66,8 @@ EVENTS = [
 def setup_driver(headless=True):
     options = Options()
 
-    if headless:
-        options.add_argument("--headless=new")
+    #if headless:
+    #    options.add_argument("--headless=new")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
