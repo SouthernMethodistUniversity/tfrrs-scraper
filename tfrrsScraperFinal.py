d773b53cc4a0cc009a6a9812645c3a98b1091ea7
Mateo Langston Smith <mlangstonsmith@smu.edu>
2025-07-25 15:07:24 -0500

    Update tfrrsScraperFinal.py

--- tfrrsScraperFinal.py
+++ tfrrsScraperFinal.py
@@ -379,7 +379,7 @@ def scrape_iterator(input_csv, output_csv):
 full_df = pd.read_csv("gendered_meets_01102025.csv")
 #full_df = full_df[::2]
 #full_df.to_csv('adj.csv', index=False)
-trial_df = full_df[7000:7002]
+trial_df = full_df[6970:7002]
 trial_df.to_csv('trial_run.csv', index=False)
 
 scrape_meets("trial_run.csv", "trial_results.csv")
